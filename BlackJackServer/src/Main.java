c4f1285b2f2a5b64739981f1f402280f7addc3e9
Gerson <Gerson@GersonPC>
2012-09-24 23:39:20 -0300

    Teste Comit

---
+++ BlackJackServer/src/Main.java
@@ -0,0 +1,47 @@
+import javax.swing.JOptionPane;
+
+import Jogador.JogadorFactory;
+import Jogador.Jogador;
+import Jogo.Jogo;
+import Jogo.JogoFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JogoFactory jogoFactory = new JogoFactory(criarJogo());
+		
+		int jogarNovamente = 0;
+		do {
+			try {
+				System.out.println("ssss");
+				Jogo jogo = jogoFactory.getJogo();
+				jogo.iniciar();
+		
+				jogo.novoTurno();
+	
+				JOptionPane.showMessageDialog(null, jogo.finalizar());
+			
+				jogarNovamente = JOptionPane.showConfirmDialog(null, "Deseja jogar novamente?");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		
+		} while(jogarNovamente == 0);
+	}
+	
+	public static Jogo criarJogo() {
+		Jogo jogo = new Jogo();
+		
+		String nome = JOptionPane.showInputDialog("Digite seu nome:");
+		Jogador jogador1 = JogadorFactory.getJogador(nome);
+		jogo.adicionarJogador(jogador1);
+
+		Jogador jogador2 = JogadorFactory.getJogador("CPU");		
+		jogo.adicionarJogador(jogador2);
+
+		return jogo;
+
+		
+	}
+
+}
